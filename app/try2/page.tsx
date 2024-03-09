173a5a43130ca96acdc903a2f73a51c83917aecc
David T <matkatdavid@gmail.com>
2024-03-08 17:43:04 -0700

    filtered by emphasis

--- app/try2/page.tsx
+++ app/try2/page.tsx
@@ -5,18 +5,23 @@ import { Container, Stack } from "@mui/material";
 import React, { useEffect, useMemo, useState } from "react";
 
 type MainPageProps = {};
+type EmphasisGroup = {
+  emphasis: string;
+  courses: Course[];
+};
 
 const MainPage = ({}: MainPageProps) => {
-  const [classes, setClasses] = useState([]);
+  const [classes, setClasses] = useState<Course[]>([]);
   const [emphases, setEmphases] = useState([""]);
+  const [classGroups, setClassGroups] = useState<EmphasisGroup[]>([]);
 
   useMemo(() => {
-    // Fetch quotes from the external JSON file
+    // Fetch classes from the external JSON file
     fetch("/classes.json")
       .then((response) => response.json())
       .then((data) => {
         setClasses(data);
-        // Extract unique categories
+        // Extract unique emphases
         const uniqueEmphases = [
           ...new Set<string>(data.flatMap((Course: Course) => Course.emphasis)),
         ];
@@ -25,9 +30,27 @@ const MainPage = ({}: MainPageProps) => {
       .catch((error) => console.error("Error fetching classes:", error));
   }, []);
 
+  useEffect(() => {
+    // Group classes by emphasis
+    const emphasisGroups = emphases.map((emphasis: string) => {
+      const courses = classes.filter(
+        (course: Course) => course.emphasis === emphasis
+      );
+      return { emphasis, courses };
+    });
+    setClassGroups(emphasisGroups);
+  }, [emphases, classes]);
+
   return (
     <Stack justifyContent={"center"} m={"0 auto"}>
-      <ClassCardList courses={classes} categoryName="Core Classes" />
+      {classGroups.map((emphasisGroup: EmphasisGroup) => (
+        <ClassCardList
+          key={emphasisGroup.emphasis}
+          courses={emphasisGroup.courses}
+          categoryName={emphasisGroup.emphasis}
+        />
+      ))}
+      {/* <ClassCardList courses={classes} categoryName="Core Classes" /> */}
     </Stack>
   );
 };
