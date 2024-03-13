fa8a262a84d53c38a327142003151ce774ba5906
tanner england <112410502+tannerengland@users.noreply.github.com>
2024-03-13 17:47:45 +0000

    dialog up & running, exit button not working, need to fix spacing for the credit hours, emphasis, prereqs, & ranking

--- components/ClassDialog.tsx
+++ components/ClassDialog.tsx
@@ -1,18 +1,59 @@
-import { Course } from "@/public/types";
-import { Dialog, Paper } from "@mui/material";
 import React from "react";
+import { Dialog, Paper, Typography, Button, Grid } from "@mui/material";
+import { Course } from "@/public/types";
 
 type ClassDialogProps = {
   showDialog: boolean;
+  onClose: () => void;
   course: Course;
 };
 
-export const ClassDialog = ({ showDialog, course }: ClassDialogProps) => {
+export const ClassDialog = ({
+  showDialog,
+  onClose,
+  course,
+}: ClassDialogProps) => {
   return (
-    <Dialog open={showDialog}>
-      <Paper>
-        <h1>{course.courseName}</h1>
-        <p>Dialog Content</p>
+    <Dialog open={showDialog} onClose={onClose}>
+      <Paper sx={{ padding: "1.5rem", position: "relative" }}>
+        <Button
+          onClick={onClose}
+          sx={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+          }}
+        >
+          X
+        </Button>
+        <Typography mb="1rem" variant="h4" fontWeight={500} fontSize="1rem">
+          <h1>
+            {course.courseName} - {course.courseTitle}
+          </h1>
+          <p>{course.description}</p>
+          <Grid container spacing={0}>
+            <Grid item xs={6}>
+              <p>
+                <strong>Credit Hours:</strong> {course.creditHours}
+              </p>
+            </Grid>
+            <Grid item xs={6}>
+              <p>
+                <strong>Prerequisites:</strong> {course.prerequisites}
+              </p>
+            </Grid>
+            <Grid item xs={6}>
+              <p>
+                <strong>Emphasis:</strong> {course.emphasis}
+              </p>
+            </Grid>
+            <Grid item xs={6}>
+              <p>
+                <strong>Ranking:</strong> {course.ranking}
+              </p>
+            </Grid>
+          </Grid>
+        </Typography>
       </Paper>
     </Dialog>
   );
